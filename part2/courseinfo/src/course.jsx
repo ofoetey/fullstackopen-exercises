69a6c703eeb7525a14e0813ace4c9f6a117b965d
Denfu <denfu.oscar.dev@gmail.com>
2025-11-18 12:14:46 +0000

    add total exercises

--- part2/courseinfo/src/course.jsx
+++ part2/courseinfo/src/course.jsx
@@ -3,6 +3,7 @@ export const Course = ({ course }) => {
     <div className="">
       <Header title={course.name} />
       <Content parts={course.parts} />
+      <TotalExercises parts={course.parts} />
     </div>
   );
 };
@@ -27,4 +28,9 @@ export const Content = ({ parts }) => {
       })}
     </>
   );
+};
+
+export const TotalExercises = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <strong>total of {total} exercises</strong>;
 };
